d1e7e6ecc420921e27a843ff0aca41dec77cae75
Patrick Geneva <patfgeneva@gmail.com>
2021-12-03 21:12:08 -0500

    update content configs

--- docs/crontab_script_launcher.sh
+++ docs/crontab_script_launcher.sh
@@ -9,7 +9,7 @@ fi
 
 # check if the process is running or not
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if pgrep -f "python3.6 $CWD/../$1" >/dev/null 2>&1 ; 
+if pgrep -f "python3 $CWD/../$1" >/dev/null 2>&1 ; 
 then
 	echo "script already running, skipping"
 	exit 1
@@ -19,4 +19,4 @@ fi
 DATE=$(date +\%Y-\%m-\%d)
 TIME=$(date +\%H-\%M-\%S)
 mkdir -p "$CWD/../logs/$DATE/"
-python3.6 "$CWD/../$1" "${@:2}" &> "$CWD/../logs/$DATE/$TIME-$1.log"
+python3 "$CWD/../$1" "${@:2}" &> "$CWD/../logs/$DATE/$TIME-$1.log"
