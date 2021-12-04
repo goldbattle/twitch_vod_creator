e296d873b0104aa0c4fe02cdab4998c4b652727f
Patrick Geneva <patfgeneva@gmail.com>
2021-12-03 21:13:25 -0500

    add single video script downloaders from youtube and update twitch archives to use gdrive

--- 0_single_video_twitcharchives.py
+++ 0_single_video_twitcharchives.py
@@ -3,6 +3,7 @@
 import twitch  # pip install python-twitch-client
 import yaml  # pip install PyYAML
 from youtube_dl import YoutubeDL # pip install youtube_dl
+from google_drive_downloader import GoogleDriveDownloader as gdd # pip install googledrivedownloader
 
 import os
 import sys
@@ -10,6 +11,7 @@ import json
 import time
 import requests
 import subprocess
+import shutil
 from datetime import datetime
 import utils
 
@@ -29,6 +31,7 @@ path_base = os.path.dirname(os.path.abspath(__file__))
 # path_twitch_ffmpeg = path_base + "/thirdparty/Twitch_Downloader_1.39.4/ffmpeg.exe"
 path_twitch_ffmpeg = path_base + "/thirdparty/ffmpeg-4.3.1-amd64-static/ffmpeg"
 path_root = path_base + "/../data/"
+path_temp = "/tmp/tvc_single_video_twitcharchives/"
 
 # ================================================================
 # ================================================================
@@ -47,12 +50,15 @@ assert (len(videos) == 1)
 # create the video object with all our information
 # DATA: api data of this vod
 video = videos[0]
+m, s = divmod(video['length'], 60)
+h, m = divmod(m, 60)
+durationstr = format(h, '02') + 'h' + format(m, '02') + 'm' + format(s, '02') + 's'
 video_data = {
     'id': str(video['vodId']),
     'user_id': str(video['channelId']),
     'user_name': video['channelName'],
     'title': video['title'],
-    'duration': -1,
+    'duration': durationstr,
     'game': "",
     'url': "https://www.twitch.tv/videos/"+str(video['vodId']),
     'views': -1,
@@ -61,6 +67,8 @@ video_data = {
     'recorded_at': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(video['created'])),
     'twitcharchives': {
         "id": video['id'],
+        "gdriveVideo": video['videoFile'],
+        "gdriveChat": video['chatFile'],
         "youtubeVideo": video['videoYoutubeId'],
         "youtubeChat": video['chatYoutubeId'],
     }
@@ -71,6 +79,10 @@ video_data = {
 path_data = path_root + "/" + video_data['user_name'].lower() + "/"
 if not os.path.exists(path_data):
     os.makedirs(path_data)
+if os.path.exists(path_temp) and os.path.isdir(path_temp):
+    shutil.rmtree(path_temp)
+if not os.path.exists(path_temp):
+    os.makedirs(path_temp)
 print("saving into " + video_data['user_name'].lower() + " user folder")
 
 # extract what folder we should save into
@@ -92,18 +104,71 @@ if not utils.terminated_requested and not os.path.exists(file_path_info):
 
 # VIDEO: check if the file exists
 file_path = path_data + export_folder + str(video_data['id']) + ".mp4"
+file_path_tmp = path_temp + str(video_data['id']) + ".tmp.mp4"
+file_path_tmp_ytdl = path_temp + str(video_data['id']) + ".tmp.%(ext)s"
 print("download video: " + file_path)
-if not utils.terminated_requested and not os.path.exists(file_path):
-    youtube_url = 'https://youtu.be/'+video_data["twitcharchives"]["youtubeVideo"]
-    print("downloading from youtube: "+youtube_url)
-    ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'recodevideo': 'mp4',
-        'outtmpl': path_data + export_folder + str(video_data['id']) + '.%(ext)s',
-    }
-    ydl = YoutubeDL(ydl_opts)
-    retcode = ydl.download([youtube_url])
-    print("\nreturn code: "+str(retcode))
+# if not utils.terminated_requested and not os.path.exists(file_path):
+
+#     # split video into the parts (if over 10 hours it splits)
+#     parts = video_data["twitcharchives"]["youtubeVideo"].split(",")
+
+#     # first lets see if we need to download multiple parts
+#     # example: sZ6u0r-SHNs,fTn4eIGpOQE
+#     for idx, part in enumerate(parts):
+#         # create filename if needed
+#         tmp_output_file_ytdl = path_temp + str(video_data['id']) + "_" + str(idx) + ".%(ext)s"
+#         if len(parts) == 1:
+#             tmp_output_file_ytdl = file_path_tmp_ytdl
+#         # download the youtube video
+#         youtube_url = 'https://youtu.be/'+part
+#         print("part "+str(idx)+": downloading from youtube: "+youtube_url)
+#         ydl_opts = {
+#             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
+#             'recodevideo': 'mp4',
+#             'outtmpl': tmp_output_file_ytdl,
+#         }
+#         ydl = YoutubeDL(ydl_opts)
+#         retcode = ydl.download([youtube_url])
+#         print("\npart "+str(idx)+": return code: "+str(retcode))
+
+
+#     # if there are multiple parts lets stitch it all together into a single video
+#     # otherwise we just need to move it to the file location
+#     # https://stackoverflow.com/a/36045659
+#     if not utils.terminated_requested and len(parts) != 1:
+#         # text file will all segments
+#         text_file_temp_videos = path_temp + "videos.txt"
+#         with open(text_file_temp_videos, 'w') as the_file:
+#             for idx, part in enumerate(parts):
+#                 tmp_output_file = path_temp + str(video_data['id']) + "_" + str(idx) + ".mp4"
+#                 the_file.write('file \'' + os.path.abspath(tmp_output_file) + '\'\n')
+#         # now render
+#         t0 = time.time()
+#         print("\t- combining all videos into a single segment!")
+#         cmd = path_twitch_ffmpeg + ' -hide_banner -loglevel quiet -stats ' \
+#               + '-f concat -safe 0 ' \
+#               + ' -i ' + text_file_temp_videos \
+#               + ' -c copy -avoid_negative_ts make_zero ' \
+#               + file_path_tmp
+#         #print(cmd)
+#         subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
+#         #subprocess.Popen(cmd, shell=True).wait()
+
+#         # end timing
+#         print("\t- time to render: " + str(time.time()-t0))
+
+#     # finally copy temp file to new location
+#     if not utils.terminated_requested and os.path.exists(file_path_tmp):
+#         print("\t- renaming temp export file to final filename")
+#         shutil.move(file_path_tmp, file_path)
+if not utils.terminated_requested and not os.path.exists(file_path) and video_data["twitcharchives"]["gdriveVideo"]:
+    gdd.download_file_from_google_drive(file_id=video_data["twitcharchives"]["gdriveVideo"], dest_path=file_path)
+
+# try to download chat if we have it
+file_path_chat = path_data + export_folder + str(video_data['id']) + "_chat.json"
+print("download chat: " + file_path_chat)
+if not utils.terminated_requested and not os.path.exists(file_path_chat) and video_data["twitcharchives"]["gdriveChat"]:
+    gdd.download_file_from_google_drive(file_id=video_data["twitcharchives"]["gdriveChat"], dest_path=file_path_chat)
 
 
 # CHAT VIDEO: check if the file exists
@@ -118,4 +183,7 @@ if not utils.terminated_requested and not os.path.exists(file_path):
 #     subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
 
 
+# cleanup temp folder
+if os.path.exists(path_temp) and os.path.isdir(path_temp):
+    shutil.rmtree(path_temp)
 
---
+++ 0_single_video_youtube.py
@@ -0,0 +1,115 @@
+# !/usr/bin/env python3
+
+import yaml  # pip install PyYAML
+from youtube_dl import YoutubeDL # pip install youtube_dl
+
+import os
+import sys
+import json
+import time
+import requests
+import subprocess
+from datetime import datetime
+from datetime import timedelta
+import utils
+
+
+# the vod which we wish to download
+if len(sys.argv) != 3:
+    print("please pass at least a user and youtube hash...")
+    exit(-1)
+channel = sys.argv[1]
+vod_yt_hash = sys.argv[2]
+
+
+# ================================================================
+# ================================================================
+
+# paths of the cli and data
+path_base = os.path.dirname(os.path.abspath(__file__))
+# path_twitch_ffmpeg = path_base + "/thirdparty/Twitch_Downloader_1.39.4/ffmpeg.exe"
+path_twitch_ffmpeg = path_base + "/thirdparty/ffmpeg-4.3.1-amd64-static/ffmpeg"
+path_root = path_base + "/../data/"
+
+# ================================================================
+# ================================================================
+
+
+# setup control+c handler
+utils.setup_signal_handle()
+
+
+# create the video object with all our information
+# DATA: api data of this vod
+youtube_url = 'https://youtu.be/'+vod_yt_hash
+print("getting video data from " + youtube_url)
+ydl = YoutubeDL()
+info_dict = ydl.extract_info(youtube_url, download=False)
+
+
+# Record the data
+video_date = datetime.strptime(info_dict.get('upload_date', None), "%Y%m%d")
+date_in_sec = (video_date-datetime(1970,1,1)).total_seconds()
+video_data = {
+    'id': str(-1),
+    'user_name': channel,
+    'title': info_dict.get('title', None),
+    'duration': time.strftime('%Hh%Mm%Ss', time.localtime(info_dict.get('duration', None))), #str(timedelta(seconds=info_dict.get('duration', None))),
+    'game': "",
+    'url': "",
+    'views': info_dict.get('view_count', None),
+    'moments': info_dict.get('chapters', None),
+    'muted_segments': [],
+    'recorded_at': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(date_in_sec)),
+    'youtube': {
+        "youtubeVideo": info_dict.get("id", None),
+        "description": info_dict.get("description", None),
+        "uploader": info_dict.get("uploader", None),
+        "uploader_url": info_dict.get("uploader_url", None),
+        "uploader_url": info_dict.get("uploader_url", None),
+    }
+}
+
+
+# check if the directory is created
+path_data = path_root + "/" + channel.lower() + "/"
+if not os.path.exists(path_data):
+    os.makedirs(path_data)
+print("saving into " + channel.lower() + " user folder")
+
+
+# extract what folder we should save into
+# create the folder if it isn't created already
+try:
+    export_folder = format(video_date.year, '02') + "-" + format(video_date.month, '02') + "/"
+except:
+    export_folder = "unknown/"
+if not os.path.exists(path_data + export_folder):
+    os.makedirs(path_data + export_folder)
+
+
+# VIDEO: check if the file exists
+file_path_info = path_data + export_folder + str(vod_yt_hash) + "_info.json"
+print("saving video info: " + file_path_info)
+if not utils.terminated_requested and not os.path.exists(file_path_info):
+    with open(file_path_info, 'w', encoding="utf-8") as file:
+        json.dump(video_data, file, indent=4)
+
+
+# VIDEO: check if the file exists
+file_path = path_data + export_folder + str(vod_yt_hash) + ".mp4"
+print("download video: " + file_path)
+if not utils.terminated_requested and not os.path.exists(file_path):
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'recodevideo': 'mp4',
+        'outtmpl': path_data + export_folder + str(vod_yt_hash) + '.%(ext)s',
+    }
+    ydl = YoutubeDL(ydl_opts)
+    retcode = ydl.download([youtube_url])
+    print("\nreturn code: "+str(retcode))
+
+
+
+
+
